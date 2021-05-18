214cb450539f56f07a1575cc3a41f428fa37b103
basilioarth <basilio.arth@gmail.com>
2021-05-18 11:07:15 -0300

    Atualizando a busca gulosa

--- src/algoritmos/heuristica-gulosa.ts
+++ src/algoritmos/heuristica-gulosa.ts
@@ -1,33 +1,30 @@
 import Result from './models/result';
 import Node from "./models/Node";
 
-export const heuristicaGulosa = (root: Node): Result => {
+const heuristicaGulosa = (root: Node): Result => {
+
     let open: Node[] = [];
     let closed: Node[] = [];
-    let custoTempo: number = 1;
-    let custoMemoria: number = 0;
     let current: Node = root;
-    let limit = 99;
+    let custoMemoria = 1;
+    let custoTempo = 0;
     let nosGerados = 0;
     let profundidade = 0;
     let profundidadeMaxima = 0;
-
+    
+    root.calcFScoreGreedy();
     open.push(root);
-    let goalFound: boolean = false;
 
-    while(open.length > 0 && !goalFound && limit > 0) {
-        limit--;
+    while (true) {
 
-        if(open.length > custoMemoria) custoMemoria = open.length;
+        if (open.length === 0) {
+            break;
+        }
+
+        current = open[0];
 
         custoTempo++;
-
-        current = open[open.length - 1];
-        closed.push(current);
-        open.pop();
-
-        if (current.manhattan() === 0){
-            goalFound = true;
+        if (current.manhattan() === 0) {
             break;
         }
 
@@ -35,7 +32,7 @@ export const heuristicaGulosa = (root: Node): Result => {
 
         for (let i = 0; i < current.children.length; i++) {
 
-            current.children[i].calcFScoreGreedy()
+            current.children[i].calcFScoreGreedy();
             let adiciona = true;
 
             for (let j = 0; j < closed.length; j++) {
@@ -55,6 +52,12 @@ export const heuristicaGulosa = (root: Node): Result => {
             }
         }
 
+        closed.push(current);
+        open.shift();
+        if (open.length > custoMemoria) {
+            custoMemoria = open.length;
+        }
+
         open.sort((a, b) => {
             return a.fScore - b.fScore;
         })
@@ -63,7 +66,7 @@ export const heuristicaGulosa = (root: Node): Result => {
     let path: Node[] = [];
     nosGerados = closed.length + open.length;
     profundidade = current.level;
-
+    
     open.map((node: Node) => {
         if (node.level > profundidadeMaxima) profundidadeMaxima = node.level;
     })
